2c46b71fa24ec3b69bb57d0f7df90fc673cbd97e
Santiago1431 <san14311982@gmail.com>
2025-03-19 07:47:21 -0300

    Variaveis e constantes

---
+++ variaveis e contantes/src/main.kt
@@ -0,0 +1,21 @@
+const val name0 = "isadora"
+fun main() {
+    println("ola mundo!")
+
+
+    //variaveis var
+    val name = "Santiago"
+    //variaveis val
+    val Idade = 20
+
+    println(name +  " idade: \n" + Idade)
+
+    var name1 = "The rock"
+    name1 = "eurico"
+    println(name1)
+
+    val name2: String = "SANTIAGO"
+    println(name2)
+    println(name0)
+}
+
